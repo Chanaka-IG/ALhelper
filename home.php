f3c857c99cb667d9fdd6011fb4570826deca314b
chanaka prasad <igchanaka.prasad@gmail.com>
2020-06-10 12:40:53 +0530

    php file for home

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php //require_once('includes/session.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" type="text/css">
+</head>
+<body>
+    <?php require_once('includes/header.php');?>
+    <div class="info clearfix">
+         <ul>
+             <li><i class="fas fa-users"></i></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+             <li></li>
+
+         </ul>
+    </div>
+    <?php require_once('includes/footer.php');?>
+    <script src="assets/navigate.js"></script>
+</body>
+</html>
